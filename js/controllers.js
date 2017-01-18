92e780c7a0acfdf6440ed31c09ef9207cc520605
mahesh maurya <mahesh@wohlig.com>
2017-01-18 18:38:16 +0530

    micro

---
+++ js/controllers.js
@@ -0,0 +1,74 @@
+angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'angular-flexslider', 'ksSwiper'])
+
+.controller('HomeCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.changecontent("home"); //Use same name of .html file
+    $scope.menutitle = NavigationService.makeactive("Home"); //This is the Title of the Website
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+
+    $scope.mySlides = [
+        'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
+        'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
+        'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
+        'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
+    ];
+})
+.controller('BrandCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.changecontent("brand"); //Use same name of .html file
+    $scope.menutitle = NavigationService.makeactive("Brand"); //This is the Title of the Website
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+})
+.controller('Ting-SeoCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.changecontent("ting-seo"); //Use same name of .html file
+    $scope.menutitle = NavigationService.makeactive("Ting-Seo"); //This is the Title of the Website
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+})
+
+.controller('FormCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.changecontent("form"); //Use same name of .html file
+    $scope.menutitle = NavigationService.makeactive("Form"); //This is the Title of the Website
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+
+    $scope.formSubmitted = false;
+
+    $scope.submitForm = function(data) {
+        console.log(data);
+        $scope.formSubmitted = true;
+    }
+})
+
+.controller('headerctrl', function($scope, TemplateService) {
+    $scope.template = TemplateService;
+    $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+        $(window).scrollTop(0);
+    });
+    $.fancybox.close(true);
+})
+
+.controller('languageCtrl', function($scope, TemplateService, $translate, $rootScope) {
+
+    $scope.changeLanguage = function() {
+        console.log("Language CLicked");
+
+        if (!$.jStorage.get("language")) {
+            $translate.use("hi");
+            $.jStorage.set("language", "hi");
+        } else {
+            if ($.jStorage.get("language") == "en") {
+                $translate.use("hi");
+                $.jStorage.set("language", "hi");
+            } else {
+                $translate.use("en");
+                $.jStorage.set("language", "en");
+            }
+        }
+        //  $rootScope.$apply();
+    };
+
+
+})
+
+;
---
+++ js/navigation.js
@@ -0,0 +1,54 @@
+var adminURL = "";
+if (isproduction) {
+    adminURL = "http://www.wohlig.co.in/demo/index.php";
+} else {
+    adminURL = "http://localhost/demo/index.php";
+}
+
+var navigationservice = angular.module('navigationservice', [])
+
+.factory('NavigationService', function() {
+    var navigation = [{
+        name: "Home",
+        classis: "active",
+        anchor: "home",
+        subnav: [{
+            name: "Subnav1",
+            classis: "active",
+            anchor: "home"
+        }]
+    },
+    {
+        name: "Brand",
+        classis: "active",
+        anchor: "brand",
+  },
+  {
+      name: "Ting-Seo",
+      classis: "active",
+      anchor: "ting-seo",
+},
+     {
+        name: "Form",
+        classis: "active",
+        anchor: "form",
+        subnav: []
+    }];
+
+    return {
+        getnav: function() {
+            return navigation;
+        },
+        makeactive: function(menuname) {
+            for (var i = 0; i < navigation.length; i++) {
+                if (navigation[i].name == menuname) {
+                    navigation[i].classis = "active";
+                } else {
+                    navigation[i].classis = "";
+                }
+            }
+            return menuname;
+        },
+
+    };
+});
